dfbff522f5502073612dece2926363a15fd16be1
Dominoman <olaca@freemail.hu>
2024-05-21 16:54:26 +0200

    Init commit

--- run-me.sh
+++ run-me.sh
@@ -3,8 +3,16 @@
 cd "$(dirname "$0")"
 set -ex
 
-if [ ! -x ./.venv]; then
+# Csinálunk python virtuáis környezetet, ha nincs
+if [ ! -x .venv ] ; then
   python -m venv .venv
 fi
 
+# Aktiváljuk a virtuális környezetet
+source .venv/bin/activate
+
+# Csomagok installálása
+if [ -f requirements.txt ] ; then
+  pip install -r requirements.txt
+fi
 
