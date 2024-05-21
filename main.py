d3e0b27486977512a4b44ff137f16a5f00bcc69c
Dominoman <olaca@freemail.hu>
2024-05-21 10:28:59 +0200

    Init commit

---
+++ main.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import os.path
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['POST'])
+def process():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    payload = request.json
+    repository = payload["repository"]
+    print(repository["full_name"])
+    print(current_path)
+    name = repository["name"]
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0")
