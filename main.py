64cf7c99554ffd12312d8c900776ea42488b30c1
Dominoman <olaca@freemail.hu>
2024-05-21 10:38:31 +0200

    Init commit

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ app = Flask(__name__)
 @app.route('/', methods=['POST'])
 def process():
     current_path = os.path.dirname(os.path.realpath(__file__))
+    parent = os.path.dirname(os.path.realpath(os.path.join(current_path,'..')))
+    print(parent)
     payload = request.json
     repository = payload["repository"]
     print(repository["full_name"])
     print(current_path)
     name = repository["name"]
+
+    return "Ok"
 
 
 if __name__ == '__main__':
